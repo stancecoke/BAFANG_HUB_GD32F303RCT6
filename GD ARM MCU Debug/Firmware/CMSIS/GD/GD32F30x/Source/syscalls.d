a2d9d17490971991140480621fa0a0bd48c6dd5b
stancecoke <hochsitzcola@web.de>
2025-08-26 19:12:12 +0200

    delete debug output

--- GD ARM MCU Debug/Firmware/CMSIS/GD/GD32F30x/Source/syscalls.d
+++
@@ -1,2 +0,0 @@
-Firmware/CMSIS/GD/GD32F30x/Source/syscalls.o: \
- ../Firmware/CMSIS/GD/GD32F30x/Source/syscalls.c
--- GD ARM MCU Debug/gcc_startup/startup_gd32f30x_hd.d
+++
@@ -1,1 +0,0 @@
-gcc_startup/startup_gd32f30x_hd.o: ../gcc_startup/startup_gd32f30x_hd.S
