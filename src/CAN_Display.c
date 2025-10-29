609b40cab634499b42dc4e18d7d30748567708f5
stancecoke <hochsitzcola@web.de>
2025-10-29 17:59:34 +0100

    experimenting with CAN messages WIP

--- src/CAN_Display.c
+++ src/CAN_Display.c
@@ -95,7 +95,7 @@ void sendCAN_Tx(MotorParams_t* MP, MotorState_t* MS){
 			transmit_message.tx_ff = CAN_FF_EXTENDED;
 			transmit_message.tx_dlen = 8;
 			transmit_message.tx_data[0] = 0xC4;
-			transmit_message.tx_data[1] = 0x09;
+			transmit_message.tx_data[1] = 0x0D;
 			transmit_message.tx_data[2] = 0xE8;
 			transmit_message.tx_data[3] = 0x03;
 			transmit_message.tx_data[4] = 0xE2;
@@ -139,6 +139,27 @@ void sendCAN_Tx(MotorParams_t* MP, MotorState_t* MS){
 
 		case 0x6302: //to do
 			/* initialize transmit message */
+			transmit_message.tx_sfid = 0x00;
+			transmit_message.tx_efid = 0x02F83210;
+			transmit_message.tx_ft = CAN_FT_DATA;
+			transmit_message.tx_ff = CAN_FF_EXTENDED;
+			transmit_message.tx_dlen = 8;
+			transmit_message.tx_data[0] = 0x00;//battery percentage
+			transmit_message.tx_data[1] = 0x00;
+			transmit_message.tx_data[2] = 0xD6;
+			transmit_message.tx_data[3] = 0x07; //cadence
+			transmit_message.tx_data[4] = 0xF8;
+			transmit_message.tx_data[5] = 0x00;
+			transmit_message.tx_data[6] = 0x00;//range LSB
+			transmit_message.tx_data[7] = 0x00;//range MSB
+
+			/* transmit message */
+			transmit_mailbox = can_message_transmit(CAN0, &transmit_message);
+			/* waiting for transmit completed */
+			timeout = 0xFFFF;
+			while((CAN_TRANSMIT_OK != can_transmit_states(CAN0, transmit_mailbox)) && (0 != timeout)){
+				timeout--;
+				}
 			break;
 	}//end case
 }
